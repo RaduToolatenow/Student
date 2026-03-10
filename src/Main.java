9cbc75a2b4a13060b405ae3061008a2c07e30380
Dogaru Radu-Nicolae <ducuducu9.rd@gmail.com>
2026-03-10 09:32:09 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
+import Lab1.Student;
+import Lab2.Ap1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static Lab2.Ap1.generareNumere;
+import static Lab2.Ap1.Sort;
 public class Main{
     public static void main(String[] args) {
 
         Student Radu = new Student(577, "Radu", "Dogaru", "ISM");
 
         System.out.println(Radu);
+
+
+
+
+        List<Integer> x = new ArrayList();
+        List<Integer> y = new ArrayList();
+        List<Integer> xPlusY = new ArrayList(); //a
+        Set<Integer> zSet = new TreeSet(); //b
+        List<Integer> xMinusY = new ArrayList();//c
+        int p = 4;
+        List<Integer> xPlusYLimitedByP = new ArrayList(); //d
+        x=generareNumere(4,10);
+
+        y=generareNumere(7,10);
+
+        Sort(x);
+        Sort(y);
+        System.out.println(x);
+        System.out.println(y);
+        xPlusY.addAll(x);
+        xPlusY.addAll(y);
+        Sort(xPlusY);
+        System.out.println(xPlusY);
     }
+
+
+
 }
