b0576d91a86576e8794d7c7b791c31ed88d3d97a
Dogaru Radu-Nicolae <ducuducu9.rd@gmail.com>
2026-03-17 08:31:06 +0200

    .

--- src/Lab1/ExistaStudent.java
+++ src/Lab1/ExistaStudent.java
@@ -6,10 +6,7 @@ public class ExistaStudent{
 public static boolean ExistaStud(List<Student> lista, String prenume, String nume, String formatie) {
     for (Student s : lista) {
 
-        if (s.getPrenume().equals(prenume) &&
-                s.getNume().equals(nume) &&
-                s.getFormatieDeStudiu().equals(formatie)) {
-
+        if (s.getPrenume().equals(prenume) && s.getNume().equals(nume) && s.getFormatieDeStudiu().equals(formatie)) {
             return true;
         }
     }
